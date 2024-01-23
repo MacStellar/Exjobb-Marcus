527cb2f2f55c948ba957dc1435aabce378b18e84
Macstellar <mar-98@hotmail.com>
2024-01-23 13:54:10 +0100

    Start

--- backend/trupal/src/main/kotlin/app/truid/trupal/TrupalApplication.kt
+++ backend/trupal/src/main/kotlin/app/truid/trupal/TrupalApplication.kt
@@ -1,11 +1,24 @@
 package app.truid.trupal
 
+
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class TrupalApplication
 
 fun main(args: Array<String>) {
     runApplication<TrupalApplication>(*args)
+
+
+}
+
+@RestController
+class MessageController {
+    @GetMapping("/")
+    fun index() = "Hello world!"
+    fun index(@RequestParam(name = "name", defaultValue = "someone") name: String) = "Hello, $name!"
 }
