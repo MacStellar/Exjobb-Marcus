19a30652dd830757b5a51732f4458c5ca7b77e5f
Macstellar <mar-98@hotmail.com>
2024-01-24 16:07:54 +0100

    Gjort klart tutorial på kotlinlang "spring-boot-using-crudrepository" och fick det att funka med POST, GET till databasen

--- backend/trupal/src/main/kotlin/app/truid/trupal/TrupalApplication.kt
+++ backend/trupal/src/main/kotlin/app/truid/trupal/TrupalApplication.kt
@@ -3,9 +3,20 @@ package app.truid.trupal
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.stereotype.Service
+import org.springframework.jdbc.core.JdbcTemplate
+import java.util.UUID
+import org.springframework.jdbc.core.query
+import org.springframework.web.bind.annotation.*
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+import org.springframework.data.repository.CrudRepository
+import java.util.*
+
+interface MessageRepository : CrudRepository<Message, String>
 
 @SpringBootApplication
 class TrupalApplication
@@ -14,19 +25,36 @@ fun main(args: Array<String>) {
     runApplication<TrupalApplication>(*args)
 
     println("Kör program")
-
 }
 
 @RestController
-class MessageController {
+class MessageController(val service: MessageService) {
     @GetMapping("/")
-//    fun index(@RequestParam(name = "name", defaultValue = "someone") name: String) = "Hello, $name!"
-    fun index() = mutableListOf(
-        Message("1", "Hello!"),
-        Message("2", "Bojour!"),
-        Message("3", "Privet!")
-    )
+    fun index(): List<Message> = service.findMessages()
+
+    @GetMapping("/{id}")
+    fun index(@PathVariable id: String): List<Message> =
+        service.findMessageById(id)
+
+    @PostMapping("/")
+    fun post(@RequestBody message: Message) {
+        service.save(message)
+    }
 }
 
-data class Message(val id: String?, val text: String)
+@Table("messages")
+data class Message(@Id var id: String?, val text: String)
 
+@Service
+class MessageService(val db: MessageRepository) {
+    fun findMessages(): List<Message> = db.findAll().toList()
+
+    fun findMessageById(id: String): List<Message> = db.findById(id).toList()
+
+    fun save(message: Message) {
+        db.save(message)
+    }
+
+    fun <T : Any> Optional<out T>.toList(): List<T> =
+        if (isPresent) listOf(get()) else emptyList()
+}
--- backend/trupal/src/main/resources/db/changesets/init.sql
+++ backend/trupal/src/main/resources/db/changesets/init.sql
@@ -8,3 +8,11 @@ CREATE TABLE interactiontest (
                              updated_at TIMESTAMP NOT NULL,
                              interaction jsonb NOT NULL
 );
+
+
+--changeset marcusschon:interaction.2
+--precondition-sql-check expectedResult:0 SELECT COUNT(to_regclass('public.messages'));
+CREATE TABLE messages (
+    id      VARCHAR(60)  DEFAULT uuid_generate_v4() PRIMARY KEY,
+    text    VARCHAR      NOT NULL
+);
